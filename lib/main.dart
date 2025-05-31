3b9d01933c69ec1765541533f3b2ed9b443626dc
walas-sa <walaasa641@gmail.com>
2025-05-31 14:20:15 +0400

    uploadtogithub

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,6 @@ class TuneApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      debugShowCheckedModeBanner: false,
       home: SplashScreen(),
     );
   }
